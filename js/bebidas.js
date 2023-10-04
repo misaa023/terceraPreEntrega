030f565a250170c3c3e505fd114c91a23dba68c2
Esteban Madrid <53920121+misaa023@users.noreply.github.com>
2023-10-04 08:57:17 -0300

    Update bebidas.js
    agregue letra que faltaba

--- js/bebidas.js
+++ js/bebidas.js
@@ -2,6 +2,6 @@
 const bebidas = [
     { id: 1, nombre: 'Cerveza Quilmes', precio: 800, img: './Quilmes.jpeg' },
     { id: 2, nombre: 'Cerveza Brahama', precio: 900, img: './brahamaa.png' },
-    { id: 3, nombre: 'Cerveza stella', precio: 1200, img: './stella.jpeg' },
-    { id: 4, nombre: 'Cerveza heineken', precio: 1300, img: './heineken.png' },
+    { id: 3, nombre: 'Cerveza Stella', precio: 1200, img: './stella.jpeg' },
+    { id: 4, nombre: 'Cerveza Heineken', precio: 1300, img: './heineken.png' },
 ];
