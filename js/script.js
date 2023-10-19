e7307b2f988d56f17116148a70e9a70df4f3454b
Esteban Madrid <53920121+misaa023@users.noreply.github.com>
2023-10-18 21:19:19 -0300

    subiendo version final del codigo

--- js/script.js
+++ js/script.js
@@ -6,35 +6,43 @@ const modalContainer = document.getElementById('modal-container');
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 // creo mis tarjetas de las bebidas
-bebidas.forEach((producto) => {
-    let contenido = document.createElement('div');
-    contenido.className = 'contenedor';
-    contenido.innerHTML = ` 
-    <img src="${producto.img}" alt="imagenes de cervezas" class="card-cervezas">
-    <h3 class="precio">${producto.nombre}</h3>
-    <p class="precio">$ ${producto.precio} </p>
-    `;
-    cerveza.append(contenido);
+const getProductos = async () => {
+    const response = await fetch('data.json');
+    const data = await response.json();
+    console.log(data);
 
-    // creo mi boton de comprar
-    const btnComprar = document.createElement('button');
-    btnComprar.className = 'btn-comprar';
-    btnComprar.innerText = 'Comprar';
-    contenido.append(btnComprar);
+    data.forEach((producto) => {
+        let contenido = document.createElement('div');
+        contenido.className = 'contenedor';
+        contenido.innerHTML = ` 
+        <img src="${producto.img}" alt="imagenes de cervezas" class="card-cervezas">
+        <h3 class="precio">${producto.nombre}</h3>
+        <p class="precio">$ ${producto.precio} </p>
+        `;
+        cerveza.append(contenido);
 
-    // funcion que me permite realizar la compra de una bebida y lo suma al carrito
-    btnComprar.addEventListener('click', () => {
-        carrito.push({
-            id: producto.id,
-            img: producto.img,
-            nombre: producto.nombre,
-            precio: producto.precio,
+        // creo mi boton de comprar
+        const btnComprar = document.createElement('button');
+        btnComprar.className = 'btn-comprar';
+        btnComprar.innerText = 'Comrar';
+        contenido.append(btnComprar);
+
+        // funcion que me permite realizar la compra de una bebida y lo suma al carrito
+        btnComprar.addEventListener('click', () => {
+            carrito.push({
+                id: producto.id,
+                img: producto.img,
+                nombre: producto.nombre,
+                precio: producto.precio,
+            });
+            console.log(carrito);
+            // cada vez que compro un producto lo almaceno en el local storage
+            stlocal();
         });
-        console.log(carrito);
-        // cada vez que compro un producto lo almaceno en el local storage
-        stlocal();
     });
-});
+};
+
+getProductos();
 
 //  almaceno mis productos en el localstorage
 const stlocal = () => {
